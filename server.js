a4d26d9ff047aa11d3ad9f9e4c4e43858efb7b40
EddieLo <eddarlostudy@yahoo.com>
2022-05-16 19:23:16 -0700

    testing questions

--- server.js
+++ server.js
@@ -8,14 +8,47 @@ const questions = [
     type: 'list',
     name: 'choices', 
     message: 'Where would you like to start?',
-    choices:    ['View all departments', 
-                'View all roles', 
-                'View all employees', 
-                'Add a department', 
-                'Add a role', 
-                'Add an employee', 
-                'Update an employee role',
+    choices:    ['View all Departments', 
+                'View all Roles', 
+                'View all Employees', 
+                'Add Department', 
+                'Add Role', 
+                'Add Employee', 
+                'Update Employee Role',
                 'Exit'
                 ]
     },
 ]
+
+function start() {
+    inquirer.prompt(questions)
+    .then((answer) => {
+        const { choices } = answer;
+        if (choices === "View all Departments") {
+        viewAllDepartments();
+        }
+        if (choices === "View all Roles") {
+            viewAllRoles();
+        }
+        if (choices === "View all Employees") {
+            viewAllEmployees();
+        }
+        if (choices === "Add Department") {
+            addDepartment();
+        }
+        if (choices === "Add Role") {
+            addRole();
+        }
+        if (choices === "Add Employee") {
+            addEmployee();
+        }
+        if (choices === "Update Employee Role") {
+          updateEmployee();
+        }
+        if (choices === "Exit") {
+          connect.end();
+        }
+    })        
+}
+
+start();
